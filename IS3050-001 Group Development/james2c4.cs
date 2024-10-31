bc21e99b8c03b58e8fa75a84294f4119a806c7fd
Richie James <james2c4@mail.uc.edu>
2024-10-31 11:24:05 -0400

    Added HappyHalloween

--- IS3050-001 Group Development/james2c4.cs
+++ IS3050-001 Group Development/james2c4.cs
@@ -8,5 +8,11 @@ namespace IS3050_001_Group_Development
 {
     internal class james2c4
     {
+        Console.Writeline(HappyHalloween)
+    }
+    public void HappyHalloween()
+    {
+        print("richie's favorite Candy" + Candy);
+        Candy = "Almond Joys";
     }
 }
