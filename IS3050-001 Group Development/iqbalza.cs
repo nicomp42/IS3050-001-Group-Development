16ddfecd130d92ac95b7d7069df69a2fa3d47d0a
Zoha Iqbal <iqbalza@mail.uc.edu>
2024-10-31 11:19:24 -0400

    Added public method

--- IS3050-001 Group Development/iqbalza.cs
+++ IS3050-001 Group Development/iqbalza.cs
@@ -8,5 +8,10 @@ namespace IS3050_001_Group_Development
 {
     internal class iqbalza
     {
+        public static void HappyHalloween()
+        {
+            Console.WriteLine("My name is Zoha and I like skittle ");
+        }
+
     }
 }
