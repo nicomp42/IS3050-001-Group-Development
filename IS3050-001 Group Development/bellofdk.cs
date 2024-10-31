1b884d50193b2ed6dd95e20ab6dc856391d876d5
Derick-Bellofatto <Bellofdk@mail.uc.edu>
2024-10-31 11:21:29 -0400

    Added a Method which returns nothing, Prints name and candy

--- IS3050-001 Group Development/bellofdk.cs
+++ IS3050-001 Group Development/bellofdk.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,10 @@ namespace IS3050_001_Group_Development
 {
     internal class bellofdk
     {
+        public void HappyHalloween()
+        {
+            Console.WriteLine("Name: Derick Bellofatto");
+            Console.WriteLine("Favorite Candy: Whoppers");
+        }
     }
 }
