0d32640b170cdf23e07b9d380b085eeb2fb9068d
Evan Bolin <bolinen@mail.uc.edu>
2024-10-31 11:19:34 -0400

    added a public method with no parmeters and then when invoked prints my name and favorite candy

--- IS3050-001 Group Development/bolinen.cs
+++ IS3050-001 Group Development/bolinen.cs
@@ -8,5 +8,9 @@ namespace IS3050_001_Group_Development
 {
     internal class bolinen
     {
+        public void HappyHalloween()
+        {
+            Console.WriteLine("Evan Bolin and my favorite candy is M&Ms");
+        }
     }
 }
