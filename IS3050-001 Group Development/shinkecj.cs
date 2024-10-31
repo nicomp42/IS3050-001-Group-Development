0bc6f62c643af0b738fa25af94bd0d1b722fc5f8
Cam Shinker <shinkecj@mail.uc.edu>
2024-10-31 11:19:48 -0400

    I added a method that prints my name and my favorite candy.

--- IS3050-001 Group Development/shinkecj.cs
+++ IS3050-001 Group Development/shinkecj.cs
@@ -8,5 +8,9 @@ namespace IS3050_001_Group_Development
 {
     internal class shinkecj
     {
+        public void HappyHalloween()
+        {
+            Console.WriteLine("My name is Cam Shinker and my favorite candy are Twizzlers");
+        }
     }
 }
