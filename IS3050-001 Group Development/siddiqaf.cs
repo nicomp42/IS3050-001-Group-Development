8233da592ebe424aad4ccd168b2bf5603b654413
Asfia Siddiqui <siddiqaf@mail.uc.edu>
2024-10-31 11:19:27 -0400

    I added my name and my favorite candy.

--- IS3050-001 Group Development/siddiqaf.cs
+++ IS3050-001 Group Development/siddiqaf.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace IS3050_001_Group_Development
 {
     internal class siddiqaf
+    
     {
+        /// <summary>
+        /// This is about writing a code on my name and favorite candy
+        /// </summary>
+        public void HappyHalloween()
+        {
+            Console.WriteLine("My name is Asfia and my favorite candy is Twix!");
+        }
     }
 }
