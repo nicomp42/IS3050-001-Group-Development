75545bfd359c8e8b45fc684f0b4e579a3b9e1c52
Rithu Aynampudi <aynampru@mail.uc.edu>
2024-10-31 11:19:31 -0400

    added a public method called HappyHalloween, it accepts no parameters and I added my name and fav candy

--- IS3050-001 Group Development/aynampru.cs
+++ IS3050-001 Group Development/aynampru.cs
@@ -8,5 +8,15 @@ namespace IS3050_001_Group_Development
 {
     internal class aynampru
     {
+        public class HappyHallloween
+        {
+            public void HappyHalloween()
+            {
+                String name = "Rithu";
+                String favoriteCandy = "Twix";
+                Console.WriteLine("My name is Rithu and my favorite candy is twix");
+            }
+
+        }
     }
 }
