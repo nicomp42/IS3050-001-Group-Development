016f09fa442eb58e83587113ca169a7af9cc9299
Nathan Sharpe <sharpenn@mail.uc.edu>
2024-10-31 11:19:32 -0400

    I added a method that prints my name and favorite candy.

--- IS3050-001 Group Development/sharpenn.cs
+++ IS3050-001 Group Development/sharpenn.cs
@@ -8,5 +8,10 @@ namespace IS3050_001_Group_Development
 {
     internal class sharpenn
     {
+        /// <summary>
+        /// This method prints my name and favorite candy.
+        /// </summary>
+        public void HappyHaloween()
+        { Console.WriteLine("My name is Nathan Sharpe and my favorite candy is Reese's"); }
     }
 }
