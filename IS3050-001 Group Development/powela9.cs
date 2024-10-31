34f861eb152bb565a6df6e0407fdbdad5c552322
Jay Powell <powela9@mail.uc.edu>
2024-10-31 11:19:00 -0400

    Added a public method called HappyHalloween that prints my name and favorite candy.

--- IS3050-001 Group Development/powela9.cs
+++ IS3050-001 Group Development/powela9.cs
@@ -8,5 +8,9 @@ namespace IS3050_001_Group_Development
 {
     internal class powela9
     {
+        public static void HappyHalloween()
+        {
+            Console.WriteLine("My name is Jay and I like Sour Patch Kids");
+        }
     }
 }
