423fb4b819d2f36f3b953584d895558632a157de
Luke Elmore <elmorels@mail.uc.edu>
2024-10-31 11:19:32 -0400

    Made My name and gave my favorite candy

--- IS3050-001 Group Development/elmorels.cs
+++ IS3050-001 Group Development/elmorels.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,11 @@ namespace IS3050_001_Group_Development
 {
     internal class elmorels
     {
+        public void HapppyHalloween()
+        {
+            Console.WriteLine("My Name is Luke Elmore and my favorite candy is skittles");
+        }
+        
+        
     }
 }
