fc71a4bbbbe95575082a66a1fd0fb3bd67ea0b4e
gueyene <gueyene@mail.uc.edu>
2024-10-31 11:20:14 -0400

    I've added a method to print my name and fav candy.

--- IS3050-001 Group Development/gueyene.cs
+++ IS3050-001 Group Development/gueyene.cs
@@ -8,5 +8,14 @@ namespace IS3050_001_Group_Development
 {
     internal class gueyene
     {
+        /// <summary>
+        /// This method will print my name and fav candy.
+        /// </summary>
+        public void HappyHalloween()
+        {
+            Console.WriteLine("My name is Nogaye Gueye! My fav candy is Watermelon Sour Patch!");
+            
+        }
+
     }
 }
