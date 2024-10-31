0c2a42783c9515c25e0d9f18c27b1e3931b872e5
driehajl <driehajl@mail.uc.edu>
2024-10-31 11:19:54 -0400

    Made a method that writes my name and favofrite candy

--- IS3050-001 Group Development/driehajl.cs
+++ IS3050-001 Group Development/driehajl.cs
@@ -8,5 +8,10 @@ namespace IS3050_001_Group_Development
 {
     internal class driehajl
     {
+        public void HappyHalloween()
+        {
+            Console.WriteLine("Jack Driehaus");
+            Console.WriteLine("kit kats");
+        }
     }
 }
