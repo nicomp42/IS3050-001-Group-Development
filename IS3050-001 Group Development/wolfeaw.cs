a42cba56dfcc3930ef403ae25b8b3c82e4042dc6
Drew Wolfe <wolfeaw@mail.uc.edu>
2024-10-31 11:11:55 -0400

    wolfeaw favorite candy

--- IS3050-001 Group Development/wolfeaw.cs
+++ IS3050-001 Group Development/wolfeaw.cs
@@ -8,5 +8,9 @@ namespace IS3050_001_Group_Development
 {
     internal class wolfeaw
     {
+        public void HappyHalloween()
+        {
+            Console.WriteLine("Drew Wolfe and my favorite candy is Milky Way");
+        }
     }
 }
