00dac8499cbbc34e6d37276c1b80d44d256b46ce
carfornc <carfornc@mail.uc.edu>
2024-10-31 11:19:24 -0400

    Added HappyHalloween method

--- IS3050-001 Group Development/carfornc.cs
+++ IS3050-001 Group Development/carfornc.cs
@@ -8,5 +8,12 @@ namespace IS3050_001_Group_Development
 {
     internal class carfornc
     {
+     /// <summary>
+     /// Method that writes name and favorite candy.
+     /// </summary>
+       public static void HappyHalloween()
+        {
+            Console.WriteLine("My name is Nikki Carfora and my favorite candy is Andes Mints.");
+        }
     }
 }
